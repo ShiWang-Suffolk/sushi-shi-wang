524d56b9172274eeaa352dc90833115ac2ee7c0d
ShiWang-Suffolk <swang39@su.suffolk.edu>
2025-01-29 05:47:12 -0500

    Implemented Sushi command history project

--- Sushi.hh
+++ Sushi.hh
@@ -3,12 +3,12 @@
 
 class Sushi {
 private:
-    std::vector<std::string> history;  // ❶ 使用 vector<string> 代替 int
+    std::vector<std::string> history;
     static const size_t HISTORY_LENGTH;
     static const size_t MAX_INPUT;
 
 public:
-    Sushi() : history() {};  // ❷ 初始化 history 为空 vector<string>
+    Sushi() : history() {};
     std::string read_line(std::istream &in);
     bool read_config(const char *fname, bool ok_if_missing);
     void store_to_history(std::string line);
